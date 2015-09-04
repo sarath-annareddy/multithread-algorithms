2b7cb29760183fb7f3ed9c605d20688448cde900
Sarath Annareddy <sarath.annareddy@gmail.com>
2015-09-04 12:14:10 -0700

    Added print statements

--- src/main/java/org/dataapproach/multithread/algorithm/sort/MergeSort.java
+++ src/main/java/org/dataapproach/multithread/algorithm/sort/MergeSort.java
@@ -3,6 +3,8 @@ package org.dataapproach.multithread.algorithm.sort;
 import java.util.*;
 
 /**
+ * The Big-O complexity is log2(n).
+ * 
  * @author sarath
  *
  */
@@ -18,26 +20,49 @@ public class MergeSort {
 		int RUNS = 16; // how many times to grow by 2?
 
 		System.out.println("NUM_THREADS(cores): " + NUM_THREADS);
+		System.out.println("");
 
 		for (int i = 1; i <= RUNS; i++) {
 			int[] a = createRandomArray(LENGTH);
+			
+			//System.out.println("Array("+LENGTH+") elements before sort: ");
+			// Print first 10 elements
+			//printIntArray(a);
 
 			// run the algorithm and time how long it takes
 			long startTime1 = System.currentTimeMillis();
 			parallelMergeSort(a, NUM_THREADS);
 			long endTime1 = System.currentTimeMillis();
 
+			//System.out.println("Array("+LENGTH+") elements after sort: ");
+			// Print first 10 elements
+			//printIntArray(a);
+			
 			if (!isSorted(a)) {
 				throw new RuntimeException("not sorted afterward: "
 						+ Arrays.toString(a));
 			}
 
-			System.out.printf("%10d elements  =>  %6d ms \n", LENGTH, endTime1
+			System.out.printf("Time taken for %10d elements  =>  %6d ms \n", LENGTH, endTime1
 					- startTime1);
+			System.out.println("");
 			LENGTH *= 2; // double size of array for next time
 		}
 	}
 
+	// Beginning of method void printIntArray(int[])
+
+	public static void printIntArray(int[] array) {
+
+		System.out.print("{");
+		//for (int i = 0; i < array.length - 1; i++)
+		for (int i = 0; i < 10 - 1; i++)
+		System.out.print (array[i] + ", ");
+		System.out.println(array[array.length - 1] + "}");
+
+	} // E
+	
+	
 	public static void parallelMergeSort(int[] a, int NUM_THREADS) {
 		if (NUM_THREADS <= 1) {
 			mergeSort(a);
